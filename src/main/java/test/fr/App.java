99422e4d2b7fbc467a3bd5f44511e340723a8371
aymericbarge <aymeric-barge@hotmail.fr>
2022-09-29 10:36:01 +0200

    exo1,2,3

--- src/main/java/test/fr/App.java
+++ src/main/java/test/fr/App.java
@@ -6,16 +6,65 @@ public class App {
 
     public static void main(String[] args) {
         Scanner clavier = new Scanner(System.in);
-        System.out.print("Entrez un nom ");
-        String nom1 = clavier.next();
-        System.out.print("Entrez un nombre ");
-        double nombre = clavier.nextInt();
-        System.out.print("Entrez un autre nom ");
-        String nom2 = clavier.next();
-        System.out.println("Entrz un adjectif ");
-        String adjectif = clavier.next();
-        System.out.println("Il était une fois " + nom1 + " " + "qui avait " + nombre + " " + nom2 + "."
-                + "Cela le rendait très " + adjectif);
+        // System.out.print("Entrez un nom ");
+        // String nom1 = clavier.next();
+        // System.out.print("Entrez un nombre ");
+        // double nombre = clavier.nextInt();
+        // System.out.print("Entrez un autre nom ");
+        // String nom2 = clavier.next();
+        // System.out.println("Entrz un adjectif ");
+        // String adjectif = clavier.next();
+        // System.out.println("Il était une fois " + nom1 + " " + "qui avait " + nombre
+        // + " " + nom2 + "."
+        // + "Cela le rendait très " + adjectif);
+
+        // Exo 1
+        // int heures = 12;
+        // int minutes = 34;
+        // int secondes = 56;
+        // int heuresEnSecondes = 12 * 3600;
+        // int minutesEnSecondes = 34 * 60;
+        // int secondesDepuisMinuit = heuresEnSecondes + minutesEnSecondes + secondes;
+        // System.out.println("Il est " + heures + " h " + minutes + " mins " + secondes
+        // + " s.");
+        // System.out.println("Il s'est écoulé " + secondesDepuisMinuit + " secondes
+        // depuis minuit.");
+
+        // Exo2
+        // System.out.println("Entrez le nombre d'heures : ");
+        // int heures = clavier.nextInt();
+        // System.out.println("Entrez le nombre de minutes : ");
+        // int minutes = clavier.nextInt();
+        // System.out.println("Entrez le,nombre de secondes : ");
+        // int secondes = clavier.nextInt();
+        // int heuresEnSecondes = heures * 3600;
+        // int minutesEnSecondes = minutes * 60;
+        // int secondesDepuisMinuit = heuresEnSecondes + minutesEnSecondes + secondes;
+        // System.out.println("Il est " + heures + " h " + minutes + " mins " +
+        // secondes+ " s.");
+        // System.out.println("Il s'est écoulé " + secondesDepuisMinuit + "
+        // secondesdepuis minuit.");
+
+        // Exo3
+        // System.out.println("Entrez le nombre d'heures : ");
+        // int heures = clavier.nextInt();
+        // System.out.println("Entrez le nombre de minutes : ");
+        // int minutes = clavier.nextInt();
+        // System.out.println("Entrez le,nombre de secondes : ");
+        // int secondes = clavier.nextInt();
+        // int heuresEnSecondes = heures * 3600;
+        // int minutesEnSecondes = minutes * 60;
+        // int minuits = 24 * 3600;
+        // int secondesAvantMinuit = minuits - (heuresEnSecondes + minutesEnSecondes +
+        // secondes);
+        // System.out.println("Il est " + heures + " h " + minutes + " mins " +
+        // secondes + " s.");
+        // System.out.println("Il s'est écoulé " + secondesAvantMinuit + "
+        // secondesdepuis minuit.");
+
+        // Exo4
+
+        
     }
 
 }
